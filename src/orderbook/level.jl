4806225506dc75e6288568882cf2ea9495618114
femtotrader <femto.trader@gmail.com>
2019-04-24 12:05:13 +0200

    Update level.jl

--- src/orderbook/level.jl
+++ src/orderbook/level.jl
@@ -6,6 +6,6 @@ end
 import Base: isless
 isless(l1::Level, l2::Level) = isless(l1.price, l2.price)
 
-function isunique(v_lev::Array{EOTrader.Level{Tprice,Tvolume},1}) where {Tprice,Tvolume}
+function isunique(v_lev::Array{Level{Tprice,Tvolume},1}) where {Tprice,Tvolume}
     length(unique([lv.price for lv in v_lev])) == length(v_lev)
 end
